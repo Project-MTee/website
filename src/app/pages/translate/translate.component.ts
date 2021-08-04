01f573ada4a2cd36cf8e751680b7d292112c6644
Endijs Titomers <endijs.titomers@Tilde.lv>
2021-08-04 10:20:04 +0300

    allowed file types

--- src/app/pages/translate/translate.component.ts
+++ src/app/pages/translate/translate.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ITldTranslateCoreConfig } from 'tld-translate/lib/modules/tld-common/models';
+import { ITldTranslateCoreConfig, ITldTranslateFileConfig } from 'tld-translate/lib/modules/tld-common/models';
 
 @Component({
   selector: 'app-translate',
@@ -12,6 +12,10 @@ export class TranslateComponent implements OnInit {
     clientId: "u-6c2a15a4-a048-46dd-861e-5c70d87313c1",
     loadLanguageFilesManually: false,
     appName: "Translator"
+  }
+  fileConfig: ITldTranslateFileConfig = {
+    allowedFileTypes: [".docx", ".xls", ".odt", ".tmx", ".pptx", ".txt"],
+    allowedFileTypesAuthUser: [".docx", ".xls", ".odt", ".tmx", ".pptx", ".txt"]
   }
 
   constructor() { }
