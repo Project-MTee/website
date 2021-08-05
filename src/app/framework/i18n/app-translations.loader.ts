e7bf7cba8bde54aef0fc9c93b7873322db246a10
Endijs Titomers <endijs.titomers@Tilde.lv>
2021-08-05 09:03:58 +0300

    font size for titles

--- src/app/framework/i18n/app-translations.loader.ts
+++ src/app/framework/i18n/app-translations.loader.ts
@@ -3,8 +3,8 @@ import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
 
 export function AppTranslationsLoader(http: HttpClient) {
   const loadDirectories = [
-    { prefix: './assets/i18n/', suffix: '.json' },
-    { prefix: './assets/i18n/tld-translate/', suffix: '.json' }
+    { prefix: './assets/i18n/tld-translate/', suffix: '.json' },
+    { prefix: './assets/i18n/', suffix: '.json' }
   ];
   return new MultiTranslateHttpLoader(http, loadDirectories);
 }
