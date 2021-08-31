b1933038d0fc8be4523d02102bc7fb785f6a1eca
Endijs Titomers <endijs.titomers@Tilde.lv>
2021-08-31 22:09:23 +0300

    dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 echo PUBLIC_CLIENT_ID: $PPUBLIC_CLIENT_ID
 
 # Replace configuration values from ENV variables
-envsubst < /usr/share/nginx/html/assets/config.local.json > /usr/share/nginx/html/assets/config.json.temp
-mv -f /usr/share/nginx/html/assets/config.local.json.temp /usr/share/nginx/html/assets/config.json
+envsubst < /usr/share/nginx/html/assets/config.json > /usr/share/nginx/html/assets/config.json.temp
+mv -f /usr/share/nginx/html/assets/config.json.temp /usr/share/nginx/html/assets/config.json
 
 exec "$@"
